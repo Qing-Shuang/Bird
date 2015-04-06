bc4f1e79e842f7745e99b87d1548800c8341146c
QingShuang <wumeng1118@gmail.com>
2015-04-06 14:40:22 +0800

    文章目录自动生成的脚本编写

---
+++ js/bird.js
@@ -0,0 +1,17 @@
+	window.onload = function(){
+		var h2Arrays = document.getElementsByTagName("h2");
+		if(h2Arrays.length > 1 && h2Arrays[0].textContent){
+			var mul = document.createElement("ul");
+			for(var i = 0;i < h2Arrays.length;++i){
+				var mli = document.createElement("li");
+				mli.textContent = h2Arrays[i].textContent;
+				mul.appendChild(mli);
+			}
+			var mdiv = document.createElement("div");
+			mdiv.id = "calcOfArticle";
+			mdiv.appendChild(mul);
+
+			var mbody = document.getElementsByTagName("body");
+			mbody[0].insertBefore(mdiv,h2Arrays[0]);
+		}
+	}
