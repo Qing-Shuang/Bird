77ab73ebeddda6344c77bc87e435aeba2eb42f7f
QingShuang <wumeng1118@gmail.com>
2015-04-06 15:24:48 +0800

    生成目录中加入了-目录-标题

--- js/bird.js
+++ js/bird.js
@@ -2,6 +2,8 @@
 		var h2Arrays = document.getElementsByTagName("h2");
 		if(h2Arrays.length > 1 && h2Arrays[0].textContent){
 			var mul = document.createElement("ul");
+			var mtx = document.createTextNode("Ŀ¼");
+			mul.appendChild(mtx);
 			for(var i = 0;i < h2Arrays.length;++i){
 				var mli = document.createElement("li");
 				mli.textContent = h2Arrays[i].textContent;
